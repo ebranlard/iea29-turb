b874ec051c879c31183fe463382d0dfb38471626
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2020-06-04 20:20:50 -0600

    Initial commit

---
+++ GenerateTurbBox.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+from pathlib import Path
+import matplotlib.pyplot as plt  # matplotlib for some plotting
+import numpy as np  # numeric python functions
+import pandas as pd  # need this to load our data from the csv files
+from pyconturb import gen_turb, gen_spat_grid, TimeConstraint  # functions we need from PyConTurb
+import pickle
+# from pybra.tictoc import Timer
+
+
+# ---- Parameters
+con_file = '35Hz_data/A1_pyConTurb_tc.csv'
+out_file = 'A1.pkl'
+ymin,ymax = -240,240
+zmin,zmax = 0,240
+ny = 160 # TODO 160
+nz = 80 # TODO 80
+h_hub=57;
+u_ref=8.42;
+
+x_refPoints=np.array(3*[293.9])
+y_refPoints=np.array(3*[107.1])
+z_refPoints=np.array([17,57,93])
+
+
+ ## Constraining time series
+con_tc = TimeConstraint(pd.read_csv(con_file, index_col=0))  # load data from csv directly into tc
+con_tc.index = con_tc.index.map(lambda x: float(x) if (x not in 'kxyz') else x)  # index cleaning
+# con_tc.iloc[:7, :]  # look at the first 7 rows
+# con_tc.get_spat()
+# con_tc.get_time().iloc[:5, :8]
+dt = con_tc.get_time().index[1]
+T  = con_tc.get_T()-dt # TODO
+print('>>>',T,dt)
+
+
+# We can plot the points to visualize the locations of the constrainting points in space.
+# u_locs = con_tc.get_spat().filter(regex='u_').loc[['y', 'z']]
+# [plt.scatter(u_locs.loc['y', col], u_locs.loc['z', col], label=col) for col in u_locs];
+# plt.legend(); plt.ylabel('height [m]'); plt.xticks([]);
+
+
+# Now let's visualize the constraining time series.
+# time_df = con_tc.get_time()
+# ax = time_df.filter(regex='u_', axis=1).plot(lw=0.75)  # subselect long. wind component
+# ax.set_ylabel('longitudinal wind speed [m/s]');
+# [print(x) for x in time_df.filter(regex='u_', axis=1).mean()];  # print mean values
+# plt.show()
+
+
+## Inputs to constrained turbulence
+# The first step is to define the spatial information for the desired turbulence box and the related parameters for the turbulence generation technique. In this case we will use the default IEC 61400-1 Ed. 3 simulation procedures (Kaimal Spectrum with Exponential Coherence) instead of interpolating from the data. Note that, by default, PyConTurb will not interpolate profiles from data.
+
+y = np.linspace(ymin,ymax, ny)  # lateral components of turbulent grid
+z = np.linspace(zmin,zmax, nz)  # vertical components of turbulent grid
+kwargs = {'u_ref': u_ref, 'turb_class': 'B', 'z_hub': h_hub,  # necessary keyword arguments for IEC turbulence
+          'T': con_tc.get_T(), 'dt': con_tc.get_time().index[1]}  # simulation length (s) and time step (s)
+interp_data = 'none'  # use the default IEC 61400-1 profile instead of interpolating from contstraints
+
+# This function below generates the actual spatial data. It assumes we want all three turbulence components at each spatial location.
+spat_df = gen_spat_grid(y, z)  # create our spatial pandas dataframe. Columns are k, p_id x, y, and z.
+
+# plt.scatter(spat_df.loc['y'], spat_df.loc['z'], label='sim. grid')
+# plt.plot(con_tc.iloc[2, :6], con_tc.iloc[3, :6], 'rX', label='constraint')
+# plt.axis('equal'); plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.);
+
+
+#---  Generate constrained turbulence
+# We now pass our constraint object and other arguments into `gen_turb` as follows.
+sim_turb_df = gen_turb(spat_df, con_tc=con_tc, interp_data=interp_data, verbose=True, **kwargs)
+
+pickle.dump(sim_turb_df, open(out_file,'wb'))
+
+
+# As a sanity check, let's compare a simulated point that is close to a constraint point. If we did this right, they should look similar to each other.
+# find the points close to where we want to look
+yloc, zloc = y_refPoints[1], y_refPoints[2]  # location we want to compare
+isim = np.argmin((spat_df.loc['y'].values - yloc)**2+(spat_df.loc['z'].values - zloc)**2)
+icon = np.argmin((con_tc.loc['y'].values - yloc)**2+(con_tc.loc['z'].values - zloc)**2)
+t, usim, ucon = sim_turb_df.index, sim_turb_df.iloc[:, isim], con_tc.get_time().iloc[:, icon]
+# plot the time series
+plt.plot(t, usim, label='simulated')
+plt.plot(t, ucon, 'r', label='constraint')
+plt.legend();
+
+# Let's also check out statistics of $u$ by height:
+stats = sim_turb_df.filter(regex='u_', axis=1).describe().loc[['mean', 'std']]
+# plot
+plt.clf(); plt.subplot(1, 2, 1);
+plt.scatter(stats.loc['mean'], spat_df.filter(regex='u_').loc['z'], label='Mean profile')
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.scatter(stats.loc['std'], spat_df.filter(regex='u_').loc['z'], label='Std dev')
+plt.legend();
+
+
+
+# **Note**: The profile functions selected for the wind speed, turbulence standard deviation and power spectra affect whether you regenerate the constraining data if a simulation point is collocated. One option is to use the built-in profile functions that interpolates these profiles from your data (see related example in the documentation). Otherwise, you can define your own profile functions for custom interpolation.
---
+++ Layout.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# Local 
+import weio
+from pybra.colors import *
+from pybra.figure import *
+
+
+Layout={'WT1': [473398.8, 6144826, 448, 257]
+,'WT2': [473745.7, 6144692, 253, 201]
+,'WT3': [474211.7, 6144514, 559, 138]
+,'WT4': [474536.7, 6144389, 884, 128]
+,'WT5': [473488.5, 6145059, 371, 291]
+,'Ref': [473835.5, 6144928, 0  , 0]
+,'WT7': [474301.5, 6144749, 499, 111]
+,'WT8': [474627.2, 6144626, 847, 111]
+,'MM':  [473572.3, 6144759, 313, 237]}
+
+Layout_rel={
+ 'WT1': np.array([448*np.cos(-257*np.pi/180+np.pi/2), 448*np.sin(-257*np.pi/180+np.pi/2), 448, 257])
+,'MM':  np.array([313*np.cos(-237*np.pi/180+np.pi/2), 313*np.sin(-237*np.pi/180+np.pi/2), 313, 237])
+,'Ref': np.array([0,0,0  , 0])                                }
+                                                         
+                                                         
+                                                         
+def plot_layout(Layout, rose=False):                     
+    fig,ax = plt.subplots(1, 1, sharey=False, figsize=(6.4,4.8)) # (6.4,4.8)
+    fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
+    for k,v in Layout.items():                           
+        if k[0]=='_':
+            continue
+        ax.plot(v[0],v[1], 'ko')                         
+        ax.text(v[0]+1,v[1]-2, ' '+k, ha='left',va='top', fontsize=9)
+                                                         
+                                                         
+    if rose:                                             
+        xc,yc=Layout['Ref'][0], Layout['Ref'][1]         
+        theta=np.linspace(0,2*np.pi,80)                  
+        xcirc,ycirc = 100 * np.cos(theta), 100*np.sin(theta)
+        ax.plot(xc+xcirc, yc+ycirc,'k-')                 
+        def plot_vec(ax,theta_north, R=110, sty='r-',lbl=None, Rext=0):
+            theta_north*=np.pi/180                       
+            theta=-theta_north +np.pi/2                  
+            xvec,yvec=np.array([0,R*np.cos(theta)]), np.array([0,R*np.sin(theta)])
+            ax.plot(xc+xvec, yc+yvec, sty)               
+            if lbl is not None:                          
+                R=R+Rext                                 
+                xvec,yvec=np.array([0,R*np.cos(theta)]), np.array([0,R*np.sin(theta)])
+                ax.text(xc+xvec[1],yc+yvec[1],lbl, ha='center',va='center', fontsize=10)
+        plot_vec(ax,theta_north=Layout['WT1'][3], R=Layout['WT1'][2], sty='r-', lbl=str(Layout['WT1'][3])+r'$\circ$', Rext=50)
+        plot_vec(ax,theta_north=Layout['MM'][3] , R=Layout['MM'][2], sty='g-' , lbl=str(Layout['MM'][3] )+r'$\circ$', Rext=50)
+        plot_vec(ax,theta_north=0  , R=150, sty='k--', lbl='N', Rext=10)
+        plot_vec(ax,theta_north=90 , R=150, sty='k--', lbl='E', Rext=10)
+        plot_vec(ax,theta_north=180, R=150, sty='k--', lbl='S', Rext=10)
+        plot_vec(ax,theta_north=270, R=150, sty='k--', lbl='W', Rext=10)
+    ax.set_aspect('equal')                               
+    ax.tick_params(direction='in')                       
+                                                         
+    return fig,ax                                        
+    # ax.legend()                                        
+                                                         
+def annotate_dim(ax,xyfrom,xyto,text=None, xOnly=False, yOnly=False, arrowstyle='<->'):
+    xyto=xyto.copy()
+    xyfrom=xyfrom.copy()
+    if xOnly:
+        xyto[1]=xyfrom[1]
+    elif yOnly:
+        xyto[0]=xyfrom[0]
+    distance = np.sqrt( (xyfrom[0]-xyto[0])**2 + (xyfrom[1]-xyto[1])**2 )
+
+    if text is None:
+        text = str(np.around(distance,1))+'m'
+    if arrowstyle is not None:
+        ax.annotate("",xyfrom,xyto,arrowprops=dict(arrowstyle='<->'))
+    if xOnly:
+        ax.text((xyto[0]+xyfrom[0])/2,(xyto[1]+xyfrom[1])/2,text,fontsize=9,ha='center',va='bottom')
+    elif yOnly:
+        ax.text((xyto[0]+xyfrom[0])/2,(xyto[1]+xyfrom[1])/2,text,fontsize=9,ha='left',va='center')
+    else:
+        ax.text((xyto[0]+xyfrom[0])/2,(xyto[1]+xyfrom[1])/2,text,fontsize=9,ha='center',va='bottom')
+
+
+print(Layout_rel['Ref'])
+print(Layout_rel['WT1'])
+print(Layout_rel['MM'])
+
+fig,ax = plot_layout(Layout_rel, rose=True)
+annotate_dim(ax,Layout_rel['Ref'],Layout_rel['WT1'], arrowstyle=None)
+annotate_dim(ax,Layout_rel['Ref'],Layout_rel['MM'], arrowstyle=None)
+ax.set_xlabel(r'$x_{ref}$ [m]')
+ax.set_ylabel(r'$y_{ref}$ [m]')
+ax.set_xlim([-550 ,250])
+ax.set_ylim([-250,250])
+ax.set_title('LayoutRelRef')
+
+
+fig,ax = plot_layout(Layout, rose=True)
+ax.set_xlabel(r'$x_{abs}$, Easting [m]')
+ax.set_ylabel(r'$y_{abs}$, Northing [m]')
+ax.set_xlim([473200 ,474800])
+ax.set_ylim([6144300,6145500])
+ax.set_title('LayoutAbs')
+
+
+# Rotating layout
+Layout_rot = Layout_rel.copy()
+xref,yref  = Layout_rel['WT1'][0], Layout_rel['WT1'][1]
+theta =-257*np.pi/180
+theta = -theta +np.pi/2
+c=np.cos(theta)
+s=np.sin(theta)
+for k,v in Layout_rot.items():
+    x=v[0]-xref
+    y=v[1]-yref
+    xrot = x * c  - y *s 
+    yrot = y * c  + x *s 
+    Layout_rot[k] = [xrot, yrot]
+
+ymin = -240
+ymax =  240
+xmin = -240
+xmax = 1250
+Layout_rot['_In'] = [xmin, 0]
+Layout_rot['_Up'] = [xmin, ymax]
+
+
+fig,ax = plot_layout(Layout_rot, rose=False)
+
+
+ax.plot([xmin,xmax],[ymin,ymin],'-.', c=fColrs(2), label='Box extent, y')
+ax.plot([xmin,xmax],[ymax,ymax],'-.', c=fColrs(2))
+ax.plot([xmin,xmin],[ymin,ymax],'--', c=fColrs(1), label='Inlet' )
+
+annotate_dim(ax,Layout_rot['MM'],Layout_rot['WT1'],xOnly=True)
+annotate_dim(ax,Layout_rot['MM'],Layout_rot['WT1'],yOnly=True)
+annotate_dim(ax,Layout_rot['WT1'],Layout_rot['Ref'],xOnly=True)
+annotate_dim(ax,Layout_rot['_In'],Layout_rot['WT1'],xOnly=True)
+annotate_dim(ax,Layout_rot['_In'],Layout_rot['_Up'],yOnly=True)
+
+
+ax.set_xlabel(r'$x_{box}$ [m]')
+ax.set_ylabel(r'$y_{box}$ [m]')
+ax.legend()
+# ax.set_xlim([473200 ,474800])
+# ax.set_ylim([6144300,6145500])
+ax.set_title('LayoutBox')
+
+plt.show()
+
+
+
+# For a rotor diameter of ≈80 m, a maximum chord of ≈3 m, and a wind speed of ≈8 m/s, the rules of thumb for convergence of FAST.Farm would suggest the following discretization requirements for the ambient wind field:
+# •	Spatial resolution around rotor ≤ 3 m
+# •	Time step around rotor ≤ 0.1 s
+# •	Spatial resolution for meandering (away from the rotor) ≤ 8 m
+# •	Time step for meandering (away from the rotor) ≤ 2 s
+# 
+# Based on these results, it would be easiest for us if there was one Mann box with uniform spatial-temporal resolution of ≈3 m and ≈0.1 s.  But we could probably make do (with a bit of data conversion) if you provided two boxes based on the above discretization requirements.  To ensure that it is big enough to capture wake meandering, the larger box (or one large box) should probably be around 6D (480 m) wide and 3D (240 m) tall.
+# 
+# 
